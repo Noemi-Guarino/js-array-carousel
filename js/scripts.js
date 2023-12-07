8b9f419a583b0e04a947888ecb81c1c23ef255f2
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-07 23:14:50 +0100

    compleate

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+let carouselcontainer = document.querySelector(".carousel_container");
+
+//  carouselcontainer.innerHTML +=
+//      `<div><img src="img/01.webp" alt=""></div>`;
+//       console.log(carouselcontainer)
+
+let imgItems = [
+     'img/01.webp',
+     'img/02.webp',
+     'img/03.webp',
+     'img/04.webp',
+];
+// console.log(imgItems)
+
+let active = 0;
+
+for (let i = 0 ; i < imgItems.length ; i++){
+     carouselcontainer.innerHTML +=
+     `<div class="specialclass"><img src="${imgItems[i]}" alt=""></div>`;
+     
+};
+console.log(carouselcontainer)
+
+let arrayClass = document.getElementsByClassName("specialclass");
+arrayClass[active].classList.add('active');
+
+let right = document.querySelector(".rightarrow");
+let left = document.querySelector(".leftarrow");
+
+right.addEventListener("click", function(){
+
+     if (active == imgItems.length -1) {
+          active = 0;
+     } else {
+          active++;
+     }
+ 
+     document.querySelector(".specialclass.active").classList.remove('active');
+     arrayClass[active].classList.add('active');
+
+})
+
+left.addEventListener("click", function(){
+     
+     if (active == 0) {
+          active = imgItems.length -1;
+     } else {
+          active--;
+     }
+
+     // active--; 
+     document.querySelector(".specialclass.active").classList.remove('active');
+     arrayClass[active].classList.add('active');
+})
+
